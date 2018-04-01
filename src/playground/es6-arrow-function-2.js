96f1cbcdfd8b3eb4431b5978d5a9953ef15ab782
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-03-31 18:37:27 -0700

    Fixed git push issue

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,28 +1,30 @@
 // arguments object - no longer bound with arrow functions
 
 const add = (a, b) => {
-    // console.log(arguments);
-    return a + b;
+  // console.log(arguments);
+  return a + b;
 };
 console.log(add(55, 1, 1001));
+
 // this keyword - no longer bound
 
 const user = {
-    name: 'John',
-    cities: ['Redwood City', 'Santa Cruz', 'Berkeley'],
-    printPlacesLived() {
-        return this.cities.map((city) => this.name + ' has lived in ' + city);
-    }
+  name: 'Andrew',
+  cities: ['Philadelphia', 'New York', 'Dublin'],
+  printPlacesLived() {
+    return this.cities.map((city) => this.name + ' has lived in ' + city);
+  }
 };
-
 console.log(user.printPlacesLived());
 
+// Challenge area
+
 const multiplier = {
-    numbers: [1, 2, 3, 4, 5],
-    multiplyBy: 3,
-    multiply () {
-        return this.numbers.map((multiplied) => multiplied * this.multiplyBy);
-    }
+  numbers: [10, 20, 30],
+  multiplyBy: 3,
+  multiply() {
+    return this.numbers.map((number) => number * this.multiplyBy);
+  }
 };
 
 console.log(multiplier.multiply());
--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,18 +1,22 @@
-var nameVar = 'John';
-var nameVar = 'Jim';
+var nameVar = 'Andrew';
+var nameVar = 'Mike';
 console.log('nameVar', nameVar);
 
-let nameLet = 'Janet';
-nameLet = 'Jen';
+let nameLet = 'Jen';
+nameLet = 'Julie';
 console.log('nameLet', nameLet);
 
 const nameConst = 'Frank';
 console.log('nameConst', nameConst);
 
-var fullName = 'John Cianciolo';
+// Block scoping
+
+const fullName = 'Jen Mead';
+let firstName;
+
 if (fullName) {
-    let firstName = fullName.split(' ')[0];
-    console.log(firstName);
+  firstName = fullName.split(' ')[0];
+  console.log(firstName);
 }
 
 console.log(firstName);
